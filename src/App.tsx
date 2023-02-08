a9046ecf84762f271eed5a91900d75f6146fbdd5
Dzianis Chaplia <d.chaplia@godeltech.com>
2023-02-09 00:38:10 +0100

    react-router-dom / react lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import { Counter } from "./components/Counter";
-import "./index.scss"
+import { Route, Routes, Link } from "react-router-dom";
+import "./index.scss";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPageAsync";
+import { MainPageAsync } from "./pages/MainPage/MainPageAsync";
+import { Suspense } from "react";
 export const App = () => {
   return (
     <div className="app">
-        fsfdfsf
-      <Counter />
+      <Link to={"/"}>Главная</Link>
+      <Link to={"/about"}>О нас</Link>
+      <Suspense fallback={<div>Загрузка...</div>}>
+        <Routes>
+          <Route path={"/about"} element={<AboutPageAsync />} />
+          <Route path={"/"} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,10 @@
 import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
-import { Counter } from "./components/Counter";
 
-render(<App />, document.getElementById("root"));
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
