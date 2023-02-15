6fe6739693dfba4dd94df2156fcf3392c55f1cda
Dzianis Chaplia <d.chaplia@godeltech.com>
2023-02-15 21:11:50 +0100

    theme + context

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Counter } from "./components/Counter";
 import { Route, Routes, Link } from "react-router-dom";
-import "./index.scss";
+import "./styles/index.scss";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPageAsync";
 import { MainPageAsync } from "./pages/MainPage/MainPageAsync";
 import { Suspense } from "react";
+import { useTheme } from "./theme/useTheme";
+
 export const App = () => {
+  const { theme, toggleTheme } = useTheme();
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Toggle</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О нас</Link>
       <Suspense fallback={<div>Загрузка...</div>}>
