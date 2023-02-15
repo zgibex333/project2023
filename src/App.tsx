52f044c313d2a37dc3d393743458355e49deac01
Dzianis Chaplia <d.chaplia@godeltech.com>
2023-02-15 21:38:12 +0100

    classnames function

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,12 @@ import { AboutPageAsync } from "./pages/AboutPage/AboutPageAsync";
 import { MainPageAsync } from "./pages/MainPage/MainPageAsync";
 import { Suspense } from "react";
 import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classnames/classNames";
 
 export const App = () => {
   const { theme, toggleTheme } = useTheme();
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames("app", {}, [theme])}>
       <button onClick={toggleTheme}>Toggle</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О нас</Link>
