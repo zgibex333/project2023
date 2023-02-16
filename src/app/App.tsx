601c8e503bf0329604feee72f1d6995cddcaa6e9
Dzianis Chaplia <d.chaplia@godeltech.com>
2023-02-16 14:48:53 +0100

    Router config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,8 +3,7 @@ import { Suspense } from "react";
 import { useTheme } from "app/providers/ThemeProvider";
 import { Link, Route, Routes } from "react-router-dom";
 import { classNames } from "shared/lib/classNames";
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
+import { AppRouter } from "app/providers/router";
 
 export const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -13,12 +12,7 @@ export const App = () => {
       <button onClick={toggleTheme}>Toggle</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О нас</Link>
-      <Suspense fallback={<div>Загрузка...</div>}>
-        <Routes>
-          <Route path={"/about"} element={<AboutPage />} />
-          <Route path={"/"} element={<MainPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
