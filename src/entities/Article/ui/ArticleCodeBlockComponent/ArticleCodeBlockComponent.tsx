a351a16ab48c3016589ae37bdd6adc1788f75684
Dzianis Chaplia <d.chaplia@godeltech.com>
2023-03-22 18:17:05 +0100

    loki updates

--- src/entities/Article/ui/ArticleCodeBlockComponent/ArticleCodeBlockComponent.tsx
+++ src/entities/Article/ui/ArticleCodeBlockComponent/ArticleCodeBlockComponent.tsx
@@ -2,7 +2,6 @@ import { memo } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import Code from 'shared/ui/Code/Code';
 import { ArticleCodeBlock } from '../../model/types/article';
-import cls from './ArticleCodeBlockComponent.module.scss';
 
 interface ArticleCodeBlockComponentProps {
     className?: string;
@@ -14,7 +13,7 @@ const ArticleCodeBlockComponent = memo(
         const { className, block } = props;
         return (
             <div
-                className={classNames(cls.ArticleCodeBlockComponent, {}, [
+                className={classNames('cls.ArticleCodeBlockComponent', {}, [
                     className,
                 ])}
             >
