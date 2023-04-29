da4c9da38f3802bf0c9754c4c99fed19c552bbac
Dzianis Chaplia <d.chaplia@godeltech.com>
2023-04-29 22:25:01 +0200

    circular dependancy plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -4,6 +4,7 @@ import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import CopyPlugin from 'copy-webpack-plugin';
+import CircularDependencyPlugin from 'circular-dependency-plugin';
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({
@@ -40,6 +41,12 @@ export function buildPlugins({
         plugins.push(
             new BundleAnalyzerPlugin({
                 openAnalyzer: false,
+            }),
+        );
+        plugins.push(
+            new CircularDependencyPlugin({
+                exclude: /node_modules/,
+                failOnError: true,
             }),
         );
     }
