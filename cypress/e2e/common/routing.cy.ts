dc608d251df5e5cbd596d5690ba9203cebf699d4
Dzianis Chaplia <d.chaplia@godeltech.com>
2023-05-24 00:09:38 +0200

    cypress initialization

---
+++ cypress/e2e/common/routing.cy.ts
@@ -0,0 +1,34 @@
+import selectByTestId from '../helpers/selectByTestId';
+
+describe('Routing', () => {
+    describe('Пользователь не авторизован', () => {
+        it('Переход на главную страницу', () => {
+            cy.visit('/');
+            cy.get(selectByTestId('MainPage')).should('exist');
+        });
+
+        it('Переход на страницу профиля', () => {
+            cy.visit('/profile/1');
+            cy.get(selectByTestId('MainPage')).should('exist');
+        });
+
+        it('Переход на несуществующий маршрут', () => {
+            cy.visit('/fdsfdsfsdfds');
+            cy.get(selectByTestId('NotFoundPage')).should('exist');
+        });
+    });
+
+    describe('Пользователь авторизован ', () => {
+        beforeEach(() => {
+            cy.login();
+        });
+        it('Переход на главную страницу', () => {
+            cy.visit('/profile/1');
+            cy.get(selectByTestId('ProfilePage')).should('exist');
+        });
+        it('Переход на страницу со списком статей', () => {
+            cy.visit('/articles');
+            cy.get(selectByTestId('ArticlesPage')).should('exist');
+        });
+    });
+});
---
+++ cypress/e2e/helpers/selectByTestId.ts
@@ -0,0 +1,1 @@
+export default (testId: string) => `[data-testid=${testId}]`;
